fb79b1c9a995e3f058ac74a1f06b7c02c44af9bc
petjohansson <191493+piizei@users.noreply.github.com>
2023-03-02 13:12:23 +0100

    fixing tests

--- templates/aws/main.tf
+++ templates/aws/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "s3b_test" {
   bucket = "s3-test-weu1-pj"
 }
 
-resource aws_s3_bucket_acl {
+resource "aws_s3_bucket_acl" "acl" {
   bucket = aws_s3_bucket.s3b_test.id
   acl    = "private"
 }
