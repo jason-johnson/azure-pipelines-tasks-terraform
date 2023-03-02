644b0419b28c9746cea85870ab48b44c8c26498e
petjohansson <191493+piizei@users.noreply.github.com>
2023-03-02 12:46:33 +0100

    fixing tests

--- templates/aws/main.tf
+++ templates/aws/main.tf
@@ -7,6 +7,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3b_test" {
-  bucket = "s3-test-eus-czp"
+  bucket = "s3-test-weu1-pj"
+}
+
+resource aws_s3_bucket_acl {
+  bucket = aws_s3_bucket.s3b_test.id
   acl    = "private"
 }
